7a98168d05366e14ecf41e4e3645c133e697a90d
Alex <alexedwardspencer@gmail.com>
2025-05-21 18:23:04 +0100

    Use hidden main fn in doc (#7)

--- src/quadtree.rs
+++ src/quadtree.rs
@@ -11,32 +11,32 @@ pub trait Storable<V, const N: usize> {
 /// of a quadtree that can be used for any number of dimensions.
 /// See <https://en.wikipedia.org/wiki/Quadtree> for more information.
 /// Motivation: We want to be able to store points and query regions efficiently.
-///
-/// ```
-/// use quadtree::{interval::Interval, point::Point, quadtree::QuadTree, region::Region};
-/// use std::num::NonZero;
-///         
+/// ```rust
+/// # use eyre::OptionExt;
+/// # fn main() -> eyre::Result<()> {
+/// # use quadtree::{interval::Interval, point::Point, quadtree::QuadTree, region::Region};
+/// # use std::num::NonZero;
 /// // Create a region, the bounds of the quadtree
 /// let region = Region::new(&[
-///     Interval::try_new(0.0, 10.0).unwrap(), // X-axis
-///     Interval::try_new(0.0, 10.0).unwrap(), // Y-axis
+///     Interval::try_new(0.0, 10.0)?, // X-axis
+///     Interval::try_new(0.0, 10.0)?, // Y-axis
 /// ]);
 ///
 /// // Initialise the QuadTree with this region and the maximum number of points each individual node
 /// // should store. You can store any Struct in the QuadTree as long as it implements the Storable trait.
 /// // Here we're deferring the type of the QuadTree to the compiler,
 /// // inferred from the first insert
-/// let mut quadtree = QuadTree::new(&region, NonZero::new(4).unwrap());
+/// let mut quadtree = QuadTree::new(&region, NonZero::new(4).ok_or_eyre("value must be > 0")?);
 ///
 /// // Insert points into the QuadTree
 /// for i in 0..4 {
-///     quadtree.insert(Point::new(&[i, 0])).unwrap();
+///     quadtree.insert(Point::new(&[i, 0]))?;
 /// }
 ///
 /// // To query the QuadTree, provide a region, or anything that implements the Query trait
 /// let query_region = Region::new(&[
-///     Interval::try_new(0.0, 2.0).unwrap(),
-///     Interval::try_new(0.0, 10.0).unwrap(),
+///     Interval::try_new(0.0, 2.0)?,
+///     Interval::try_new(0.0, 10.0)?,
 /// ]);
 ///
 /// let results: Vec<_> = quadtree.query(&query_region).collect();
@@ -45,6 +45,9 @@ pub trait Storable<V, const N: usize> {
 /// // Alternatively, search around a point using a DistanceQuery
 /// let distance_query = Point::new(&[5.0, 5.0]).to_distance_based_query(3.0);
 /// let results: Vec<_> = quadtree.query(&distance_query).collect();
+/// #
+/// #     Ok(())
+/// # }
 /// ```
 pub struct QuadTree<const N: usize, V> {
     region: Region<N>,
