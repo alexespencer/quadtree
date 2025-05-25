e26e0bfd0d3c92c5094386b23d0381be28ea0fea
Alex Spencer <alexedwardspencer@gmail.com>
2025-05-25 11:24:11 +0100

    Remove comments

--- visualize/src/main.rs
+++ visualize/src/main.rs
@@ -5,16 +5,13 @@ use quadtree::{interval::Interval, point::Point, quadtree::QuadTree, region::Reg
 use std::num::NonZero;
 
 fn main() -> Result<()> {
-    // Create a region, the bounds of the quadtree
+    // Create a region (the bounds of the quadtree)
     let region = Region::new(&[
         Interval::try_new(0.0, 10.0)?, // X-axis
         Interval::try_new(0.0, 10.0)?, // Y-axis
     ]);
 
-    // Initialise the QuadTree with this region and the maximum number of points each individual node
-    // should store. You can store any Struct in the QuadTree as long as it implements the Storable trait.
-    // Here we're deferring the type of the QuadTree to the compiler,
-    // inferred from the first insert
+    // Initialise the QuadTree
     let mut quadtree = QuadTree::new(&region, NonZero::new(4).ok_or_eyre("value must be > 0")?);
 
     // Insert points into the QuadTree
@@ -22,7 +19,7 @@ fn main() -> Result<()> {
         quadtree.insert(Point::new(&[i, 0]))?;
     }
 
-    // To query the QuadTree, provide a region, or anything that implements the Query trait
+    // Query quadtree
     let query_region = Region::new(&[Interval::try_new(0.0, 2.0)?, Interval::try_new(0.0, 10.0)?]);
     let results: Vec<_> = quadtree.query(&query_region).collect();
 
